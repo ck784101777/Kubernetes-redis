cdf974faed90cb7ae49b200597746da626e5654e
ck784101777 <42372966+ck784101777@users.noreply.github.com>
2023-05-29 15:29:34 +0800

    Update write.py

--- redis-cluster-3master-3slave/write.py
+++ redis-cluster-3master-3slave/write.py
@@ -12,12 +12,12 @@ while True:
   try:
     redis_client = RedisCluster(connection_pool=pool)
     if redis_client.ping():
-      logger.info('获取Redis连接实例成功',redis_client.info())
+      logger.info('get Redis connection instance successful!',redis_client.info())
     for i in range(100):
       sleep(0.05)
       key = ''.join(random.sample(ascii_letters, k=7))
       redis_client.set(key, random.randint(1, 100), ex=500)
       print('write:',key)
   except Exception as e:
-    logger.error('Redis连接异常:{str(e)},traceback={traceback.format_exc()}')
+    logger.error('Redis connection exception:{str(e)},traceback={traceback.format_exc()}')
     pool = ClusterConnectionPool(startup_nodes=startup_nodes,skip_full_coverage_check=True,socket_connect_timeout=1)
